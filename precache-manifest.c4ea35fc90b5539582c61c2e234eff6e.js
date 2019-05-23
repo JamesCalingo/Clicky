cf0957f74d91723864a7567a4b5ccb76acb38974
JC <james.calingo@gmail.com>
2019-05-23 14:14:47 -0400

    Updates

---
+++ precache-manifest.c4ea35fc90b5539582c61c2e234eff6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f0584424e0ccaa7238633dc7025aae",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "440a6a8df96186043adb",
+    "url": "/clicky/static/css/main.14f23e6d.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/clicky/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "440a6a8df96186043adb",
+    "url": "/clicky/static/js/main.b4eb67ef.chunk.js"
+  },
+  {
+    "revision": "e4363465db862e89c37e",
+    "url": "/clicky/static/js/runtime~main.c6956f21.js"
+  }
+]);
